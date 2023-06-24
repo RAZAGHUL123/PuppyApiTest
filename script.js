d38c68831f85999aed9966cc3e3e45fbc2296c68
Rafael Moreno <92047284+RAZAGHUL123@users.noreply.github.com>
2023-06-24 09:35:55 -0700

    a change

--- script.js
+++ script.js
@@ -41,6 +41,14 @@ function getRandomPlayer() {
       playerContainer.appendChild(playerName);
       playerContainer.appendChild(playerBreed);
       playerContainer.appendChild(playerImage);
+
+      // Prompt user if they want to delete the player
+      const userInput = prompt('Do you want to delete the player? (yes/no)');
+      if (userInput.toLowerCase() === 'yes') {
+        removePlayer(randomPlayer.id);
+      } else {
+        console.log('Player not deleted.');
+      }
     })
     .catch(error => {
       displayError(error.message);
@@ -49,27 +57,26 @@ function getRandomPlayer() {
 }
 
 // Function to remove a player
-function removePlayer() {
-  fetch(APIURL, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      const { success, error } = data;
-
-      if (!success) {
-        throw new Error(error?.message || 'Failed to remove player.');
-      }
-
-      playerContainer.innerHTML = '';
-    })
-    .catch(error => {
-      displayError(error.message);
-      console.error(error);
+async function removePlayer(playerId) {
+  try {
+    const response = await fetch(`${APIURL}/${playerId}`, {
+      method: 'DELETE'
     });
+    const result = await response.json();
+    const { success, error } = result;
+
+    if (!success) {
+      throw new Error(error?.message || 'Failed to remove player.');
+    }
+
+    console.log('Player successfully deleted.');
+
+    // Fetch a new random player to display
+    getRandomPlayer();
+  } catch (error) {
+    displayError(error.message);
+    console.error(error);
+  }
 }
 
 // Function to add a player
@@ -118,7 +125,7 @@ function addPlayer(event) {
 
       nameInput.value = '';
       breedInput.value = '';
-      statusSelect.value = 'field';
+      statusSelect.value = 'bench';
       imageUrlInput.value = '';
     })
     .catch(error => {
@@ -127,7 +134,37 @@ function addPlayer(event) {
     });
 }
 
-// Event listeners
 randomPlayerButton.addEventListener('click', getRandomPlayer);
 addPlayerForm.addEventListener('submit', addPlayer);
-removePlayerButton.addEventListener('click', removePlayer);
+removePlayerButton.addEventListener('click', () => {
+  const userInput = prompt('Enter the ID of the player you want to remove:');
+  if (userInput) {
+    removePlayer(userInput);
+  }
+});
+
+// Make the API call
+fetch(APIURL)
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response
+    if (data.success) {
+      // Process the data if the response is successful
+      console.log(data.data);
+    } else {
+      // Handle the error if the response is unsuccessful
+      const errorResponse = {
+        success: false,
+        error: {
+          name: "ErrorName",
+          message: "This is an error message."
+        },
+        data: null
+      };
+      console.log(errorResponse);
+    }
+  })
+  .catch(error => {
+    // Handle any network or other errors
+    console.error(error);
+  });
